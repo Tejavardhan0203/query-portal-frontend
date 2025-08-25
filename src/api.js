d712495d259b9fceb0830ca31a4696502b902d68
Tejavardhan23 <tejavardhan.2323@gmail.com>
2025-08-25 22:37:04 +0530

    add

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
-//const api = axios.create({ baseURL: import.meta.env.VITE_API || 'http://localhost:5000' })
-const api = axios.create({ baseURL: import.meta.env.VITE_API || 'https://query-backend-theta.vercel.app/' })
+const api = axios.create({ baseURL: import.meta.env.VITE_API || 'http://localhost:5000' })
+//const api = axios.create({ baseURL: import.meta.env.VITE_API || 'https://query-backend-theta.vercel.app/' })
 export default api
